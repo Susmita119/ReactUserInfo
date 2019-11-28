7a34da40884279e4fffecc243b27546d1048967c
Susmita Narkar <56936826+Susmita119@users.noreply.github.com>
2019-11-28 18:14:18 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Login from './Containers/Login/index.js'
+import SignIn from './Containers/SignIn/index.js'
 
-function App() {
+class App extends Component {
+  render(){
   return (
+    <Router> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/login'} className="nav-link">Login</Link></li>
+            <li><Link to={'/signIn'} className="nav-link">SignIn</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              
+              <Route path='/login' component={Login} />
+              <Route path='/signIn' component={SignIn} />
+          </Switch>
     </div>
+    </Router>
   );
+  }
 }
 
 export default App;
